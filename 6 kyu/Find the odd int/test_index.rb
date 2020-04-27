4e4243fb960896817534768e67c35f28b7ba35ee
Abdelrahman HSN <abdelrahmanhsn1@gmail.com>
2020-04-27 07:46:04 +0200

    Find the odd int

---
+++ 6 kyu/Find the odd int/test_index.rb
@@ -0,0 +1,12 @@
+require_relative "index"
+require "test/unit"
+
+class TestIndex < Test::Unit::TestCase
+  def test_simple
+    assert_equal(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
+    assert_equal(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1)
+    assert_equal(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5)
+    assert_equal(find_it([10]), 10)
+    assert_equal(find_it([1,1,1,1,1,1,10,1,1,1,1]), 10)
+  end
+end
